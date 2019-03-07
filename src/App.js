134355f6a08c61a44b6379db62e02f102762c2ac
mostafij163 <mostafijurrahman163@gmail.com>
2019-03-07 20:55:31 +0600

    finished change state part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-import ContactCard from "./ContactCard";
-import Qustion from './Qustion';
-import qustionData from './qustionData';
-import Product from './Product';
-import productData from './productData';
+// import ContactCard from "./ContactCard";
+// import Qustion from './Qustion';
+// import qustionData from './qustionData';
+// import Product from './Product';
+// import productData from './productData';
 import Todo from './Todo';
 import todoData from './todoData';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todos: todoData
+    }
+
+    this.taskCompleter = this.taskCompleter.bind(this);
+  }
+
+  taskCompleter(id) {
+    this.setState(prevState => {
+      const updatedTodo = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo
+      });
+      return {
+        todos: updatedTodo
+      }
+    });
+  }
+
   render() {
     //const qustionComponent = qustionData.map(data => <Qustion key={data.id} qustion={data.qustion} answer= {data.answer} />)
 
@@ -15,7 +39,8 @@ class App extends Component {
     //img= {product.img} name= {product.name} company= {product.company} price= {product.price} description= {product.description}
     //const productComponent = productData.map(items => <Product key= {items.id} product= {items} />);
 
-    const todoComponent = todoData.map(items => <Todo key= {items.id} item= {items} />);
+    const todoComponent = this.state.todos.map(items => <Todo key= {items.id} item= {items} handleChange = {this.taskCompleter} />);
+
     return (
       <div className="App">
         {todoComponent}
--- src/Todo.js
+++ src/Todo.js
@@ -4,7 +4,11 @@ function Todo(props){
 	return(
 			<div>
 				<div style={{backgroundColor: "#f0f0f0", margin: "0px auto", width: "960px"}}>
-					<input type="checkbox"/>
+					<input 
+						type="checkbox" 
+						checked = {props.item.completed}
+						onChange = {()=> props.handleChange(props.item.id)}
+					/>
 					<label style= {{display: "inline-block", padding: "10px", borderBottom: "2px solid #cccccc", width: "98%"}} >{props.item.task}</label>	
 				</div>
 			</div>
